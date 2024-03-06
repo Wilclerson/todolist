221731e5ee4511ead48b3788d150aa1dccf081a3
wraymond2017 <wraymond2017@outlook.com>
2024-03-06 17:30:36 -0500

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import PropTypes from 'prop-types'; // ES6
 
-function App() {
+
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>My Todos</h1>
+      <ul>
+        <li>
+          <input  
+              type ="text"
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                    setTodos(todos.concat([inputValue]));
+                    setInputValue("");
+                }
+              }}
+              placeholder="What do you need to do?"></input>
+        </li>
+        {todos.map((item, index) => (
+          <li>
+            {item}{" "}
+           <FontAwesomeIcon 
+           icon={fas.faTrash} 
+              onClick={() =>
+                setTodos(
+                    todos.filter(
+                        (t, currentIndex) =>
+                          index != currentIndex
+                    )
+                   )
+                }/>
+              </li>
+            ))}
+      </ul>
+    <div>{todos.length} tasks </div>
     </div>
   );
 }
-
 export default App;
